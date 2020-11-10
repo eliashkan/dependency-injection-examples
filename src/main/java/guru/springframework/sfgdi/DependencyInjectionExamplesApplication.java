d930b6f2b1a3383150b8843fc61e93c5bde7e5ed
Elias Khan <khan.elias@gmail.com>
2020-11-10 19:31:31 +0100

    externalizing properties example

--- src/main/java/guru/springframework/sfgdi/DependencyInjectionExamplesApplication.java
+++ src/main/java/guru/springframework/sfgdi/DependencyInjectionExamplesApplication.java
@@ -1,6 +1,7 @@
 package guru.springframework.sfgdi;
 
 import guru.springframework.sfgdi.controllers.*;
+import guru.springframework.sfgdi.examplebeans.FakeDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -12,35 +13,11 @@ public class DependencyInjectionExamplesApplication {
         ConfigurableApplicationContext ctx =
                 SpringApplication.run(DependencyInjectionExamplesApplication.class, args);
 
-        I18nController i18nController = (I18nController) ctx.getBean("i18nController");
-        System.out.println(":: Profile based injection ::");
-        System.out.println(i18nController.sayHello());
+        FakeDataSource source = (FakeDataSource) ctx.getBean(FakeDataSource.class);
 
-
-        MyController myController = (MyController) ctx.getBean("myController");
-
-        System.out.println(":: Primary Bean ::");
-        System.out.println(myController.sayHello());
-
-        System.out.println(":: property injection ::");
-
-        PropertyInjectedController propertyInjectedController =
-                (PropertyInjectedController) ctx.getBean("propertyInjectedController");
-
-        System.out.println(propertyInjectedController.getGreeting());
-
-        System.out.println(":: setter injection ::");
-        SetterInjectedController setterInjectedController =
-                (SetterInjectedController) ctx.getBean("setterInjectedController");
-
-        System.out.println(setterInjectedController.getGreeting());
-
-        System.out.println(":: constructor injection ::");
-
-        ConstructorInjectedController constructorInjectedController =
-                (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
-
-        System.out.println(constructorInjectedController.getGreeting());
+        System.out.println(source.getUser());
+        System.out.println(source.getPassword());
+        System.out.println(source.getUrl());
     }
 
 }
---
+++ src/main/java/guru/springframework/sfgdi/config/PropertyConfig.java
@@ -0,0 +1,36 @@
+package guru.springframework.sfgdi.config;
+
+import guru.springframework.sfgdi.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@Configuration
+@PropertySource("classpath:datasource.properties")
+public class PropertyConfig {
+
+    @Value("${guru.user}")
+    String user;
+
+    @Value("${guru.password}")
+    String password;
+
+    @Value("${guru.dburl}")
+    String url;
+
+    @Bean
+    public FakeDataSource fakeDataSource() {
+        FakeDataSource source = new FakeDataSource();
+        source.setUser(user);
+        source.setPassword(password);
+        source.setUrl(url);
+        return source;
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer properties() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+}
